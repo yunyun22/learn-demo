22208aa9aa69f1b6e08d08e75d739aab45817213
wangjq <jinqiao.wang@ca-nio.com>
2021-12-03 16:20:06 +0800

    jvm test

---
+++ demo-common/src/main/java/com/wjq/demo/common/domain/User.java
@@ -0,0 +1,19 @@
+package com.wjq.demo.common.domain;
+
+/**
+ * @author wjq
+ * @since 2021-12-03
+ */
+
+public class User {
+
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private int id;
+
+    private String name;
+}
--- demo-spring/src/main/java/com/wjq/demo/spring/Application.java
+++ demo-spring/src/main/java/com/wjq/demo/spring/Application.java
@@ -1,14 +1,8 @@
 package com.wjq.demo.spring;
 
 
-import com.wjq.demo.config.Test2;
-import com.wjq.demo.config.TestConfig;
-import com.wjq.demo.spring.config.ManConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.context.annotation.FilterType;
 
 /**
  * @author wjq
