ad7bb261995d522a60f5a9991ef36b218f913843
wangjq <jinqiao.wang@ca-nio.com>
2022-09-19 13:34:56 +0800

    ProxyInterceptor

--- demo-springcloudfeign/src/main/java/com/wjq/demo/feign/config/MyFeignConfig.java
+++ demo-springcloudfeign/src/main/java/com/wjq/demo/feign/config/MyFeignConfig.java
@@ -4,20 +4,27 @@ import com.wjq.demo.feign.intecepter.ProxyInterceptor;
 import feign.Feign;
 import feign.Request;
 import feign.Retryer;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author wjq
  * @since 2021-12-23
  */
-public class MyFeignConfig {
+public class MyFeignConfig implements ApplicationContextAware {
 
 
     private static final int CONNECT_TIME_OUT_MILLIS = 3000;
     private static final int READ_TIME_OUT_MILLIS = 3000;
+    private ApplicationContext applicationContext;
 
     @Bean
     public Request.Options options() {
@@ -31,12 +38,22 @@ public class MyFeignConfig {
 
 
     @Bean
-    public ProxyInterceptor proxyInterceptor(){
+    public ProxyInterceptor proxyInterceptor() {
+        ApplicationContext parent = applicationContext.getParent();
+        if (parent != null) {
+            Map<String, ProxyInterceptor> beansOfType = parent.getBeansOfType(ProxyInterceptor.class);
+            if (beansOfType.isEmpty()){
+                System.out.println("ProxyInterceptor is empty");
+            }
+            System.out.println("再次代理");
+        }
+
         return new ProxyInterceptor();
     }
 
-//    @Bean
-//    public Feign.Builder feignBuilder() {
-//        return new Feign.Builder().addCapability(new MyCapability());
-//    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
